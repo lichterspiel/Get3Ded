53fb0ac4bab132f76836dd2bc21dc26741af6df1
Mehmet Sinan Eris <sun@flower.fritz.box>
2020-11-30 10:57:11 +0100

    start

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+#!!!TODO!! Delete These when finished or research what they do just for debugging 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("start.html")
+
